f8e7b2b608bb0144c774a55130ee3f6d63ac0dfe
thyroscope-jihun <qkwl1013@naver.com>
2024-06-11 11:12:48 +0900

    add: add test code

---
+++ src/dd.test.js
@@ -0,0 +1,12 @@
+// This is the function we will be testing
+function add(a, b) {
+  return a + b;
+}
+
+// This is our test suite
+describe("add", () => {
+  // This is an individual test
+  it("adds two numbers correctly", () => {
+    expect(add(1, 2)).toBe(3);
+  });
+});
